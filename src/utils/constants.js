3a3b6319475429f7e890cc3d8e6f802582f84212
CodeRAFCE <sesh.io1022@gmail.com>
2026-05-22 23:20:30 +0530

    fix: update API_URL to use CORS proxy for restaurant API access

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 export const LOGO_URL = "https://marketplace.canva.com/EAGXsRURT9o/1/0/1600w/canva-yellow-and-brown-kitchen-food-logo-JrzZUwc_CLQ.jpg";
 
-export const API_URL = "https://namastedev.com/api/v1"; // listRestaurants
+export const API_URL = "https://corsproxy.io/?url=https://namastedev.com/api/v1"; // listRestaurants
